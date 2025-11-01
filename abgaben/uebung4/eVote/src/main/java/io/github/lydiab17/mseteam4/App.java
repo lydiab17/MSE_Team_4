91c9536a5eb9a2f0085a9969e16d181d4def950a
Fabian Schmitz <fabian.schmitz1@outlook.de>
2025-11-01 11:05:25 +0100

    Created a simple Maven Project with Archetype Quickstart in Folder Uebung4 to create Tests and simple Domain logic

---
+++ abgaben/uebung4/eVote/src/main/java/io/github/lydiab17/mseteam4/App.java
@@ -0,0 +1,14 @@
+package io.github.lydiab17.mseteam4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
