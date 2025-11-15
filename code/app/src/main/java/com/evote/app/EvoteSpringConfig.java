05e30aa06f6dafc8d8907fa3755b9430847b5ab5
Fabian Schmitz <fabian.schmitz1@outlook.de>
2025-11-15 11:51:33 +0100

    VotingRestController angelegt um aus dem FE per REST auf Voting zuzugreifen

--- code/app/src/main/java/com/evote/app/EvoteSpringConfig.java
+++ code/app/src/main/java/com/evote/app/EvoteSpringConfig.java
@@ -2,6 +2,10 @@ package com.evote.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hiermit werden die mit Annotierten @Servive @Repository usw. gefunden
+ * Spring startet automatisch auf dem Port 8080
+ */
 @SpringBootApplication
 public class EvoteSpringConfig {
 
