492d5b2ff5d4e44575b15060a73b67470aee0767
Fabian Laptop <fabian.schmitz1@outlook.de>
2025-12-01 19:06:42 +0100

    Falsche Importe angepasst

--- code/app/src/main/java/com/evote/app/votingmanagement/infrastructure/repositories/InMemoryVoteRepository.java
+++ code/app/src/main/java/com/evote/app/votingmanagement/infrastructure/repositories/InMemoryVoteRepository.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+
+import com.evote.app.votingmanagement.domain.model.Vote;
+import com.evote.app.votingmanagement.domain.model.VoteRepository;
 import org.springframework.stereotype.Repository;
 
 /**
